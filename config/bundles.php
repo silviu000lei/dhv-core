38016dbfa55746b02ecf1f2f9ea2c4247df847cf
Silviu Ion <silviu.ion@extendstudio.com>
2023-08-09 21:16:59 +0300

    Add cors allow all

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
